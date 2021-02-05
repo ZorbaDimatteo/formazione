0afd7d74daa570debdfa7657add6097c157a5984
robcarlucci <roberto.carlucc1997@gmail.com>
2021-02-05 15:07:08 +0100

    finse esercizi promise

--- js/promise-it-wont-hurt/multiple-promises.js
+++ js/promise-it-wont-hurt/multiple-promises.js
@@ -1,11 +1,16 @@
+'use strict';
 function all(firstPromise, secondPromise) {
-    let counter = 0;
-    let firstResult = firstPromise.then(function(){return true});
-    // console.log(counter);
-    let secondResult = secondPromise.then(function(){return true});
-    // console.log(counter);
-    return Promise.resolve([firstResult, secondResult]);
+    let result = [];
+    return new Promise((resolve, reject) => {
+        return firstPromise
+        .then((value)=> {
+            result.push(value);
+            return secondPromise;
+        }).then((value) => {
+            result.push(value);
+            resolve(result);
+        })
+    })
 }
 
-all(getPromise1(), getPromise2())
-    .then(console.log);
+all(getPromise1(), getPromise2()).then(console.log);
