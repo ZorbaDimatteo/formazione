b0a6414de1303050b95a650dbe83ff612842c9d8
robcarlucci <roberto.carlucc1997@gmail.com>
2021-02-05 12:35:33 +0100

    esercizi async

---
+++ js/async/es1.js
@@ -0,0 +1,27 @@
+/*   riscrivere questo codice utilizzando ASYNC/AWAIT
+function loadJson(url) {
+    return fetch(url)
+    .then(response => {
+        if (response.status == 200) {
+            return response.json();
+        } else {
+            throw new Error(response.status);
+        }
+    });
+}
+  
+loadJson('no-such-user.json')
+    .catch(alert); // Error: 404
+*/
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status == 200) {
+        let json = await response.json();
+        return json;
+    }
+    throw new Error(response.status);
+}
+
+loadJson('no-such-user.json')
+  .catch(alert); // Error: 404
