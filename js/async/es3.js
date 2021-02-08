b6a1ffd8a815050b648248b5a497b560b96e3062
robcarlucci <roberto.carlucc1997@gmail.com>
2021-02-08 08:37:12 +0100

    esercizi async finiti

--- js/async/es3.js
+++ js/async/es3.js
@@ -13,3 +13,15 @@
     // remember, we can't use "await"
   }
 */
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  wait().then(result => alert(result));
+}
+
+f();
